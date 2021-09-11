9257a4f8b5ab57ed949c127c840570cc61d571b5
Ricardo Tovar <ricardotovar.grupobruild@gmail.com>
2021-09-11 10:52:09 -0500

    creando endpoint controller cliente

--- api/cliente/controllers/cliente.js
+++ api/cliente/controllers/cliente.js
@@ -1,8 +1,47 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  updateClient: async (ctx) => {
+    try {
+      const data = ctx.request.body.input || ctx.request.body;
+      const user = ctx.state.user;
+      const client = await strapi.services.cliente.findOne({
+        usuario: user.id,
+      });
+
+      const cliente = await strapi.services.cliente.update(
+        {
+          id: client.id,
+        },
+        {
+          ...data.cliente,
+        }
+      );
+
+      return cliente;
+    } catch (error) {
+      throw error;
+    }
+  },
+  getProfile: async (ctx) => {
+    try {
+      const user = ctx.state.user;
+      const ownerProfile = await strapi.services.cliente.findOne({
+        usuario: user.id,
+      });
+      if (!ownerProfile) {
+        return ctx.throw(404, "auth.no_profile");
+      }
+      return {
+        cliente: ownerProfile,
+      };
+    } catch (error) {
+      throw error;
+    }
+  },
+};
